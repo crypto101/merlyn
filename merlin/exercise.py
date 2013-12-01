f3b6657ee4e529687dad796738b55728400c6a88
Laurens Van Houtven <_@lvh.cc>
2013-12-01 13:00:46 +0100

    Move a bunch of stuff out to clarent

--- merlin/exercise.py
+++ merlin/exercise.py
@@ -3,47 +3,7 @@ Basic exercise and step implementations.
 """
 from axiom import attributes, item
 from merlin import interfaces as imerlin
-from twisted.protocols import amp
-from txampext.errors import Error
 from zope.interface import implementer
-
-
-class UnknownStep(Error):
-    """
-    The step for which a submission was made was not recognized.
-    """
-
-
-
-class WrongStep(Error):
-    """
-    The step for which a submission was made was recognized, but the
-    user was not on that step of the exercise.
-
-    This can occur when a user accidentally submits something for a
-    step they had previously submitted something for.
-    """
-
-
-
-class IncorrectSubmission(Error):
-    """
-    The submission was understood, but incorrect.
-    """
-
-
-
-class Submit(amp.Command):
-    arguments = [
-        (b"step", amp.Integer()),
-        (b"submission", amp.String())
-    ]
-    response = []
-    errors = dict([
-        IncorrectSubmission.asAMP(),
-        UnknownStep.asAMP(),
-        WrongStep.asAMP()
-    ])
 
 
 
--- merlin/test/test_exercise.py
+++ merlin/test/test_exercise.py
@@ -1,31 +1,6 @@
 from merlin import exercise, interfaces as imerlin
 from twisted.trial.unittest import SynchronousTestCase
-from txampext.commandtests import CommandTestMixin
 from zope.interface.verify import verifyObject
-
-
-class SubmissionTests(SynchronousTestCase, CommandTestMixin):
-    command = exercise.Submit
-
-    argumentObjects = {
-        b"step": 1,
-        b"submission": b"xyzzy"
-    }
-    argumentStrings = {
-        b"step": "1",
-        b"submission": b"xyzzy"
-    }
-
-    responseObjects = {}
-    responseStrings = {}
-
-    errors = dict([
-        exercise.UnknownStep.asAMP(),
-        exercise.WrongStep.asAMP(),
-        exercise.IncorrectSubmission.asAMP()
-    ])
-    fatalErrors = {}
-
 
 
 class StepTests(SynchronousTestCase):
@@ -33,5 +8,5 @@ class StepTests(SynchronousTestCase):
         """
         The Step item class implements the IStep interface.
         """
-        step =  exercise.Step(text=u"", validatorName=b"")
+        step = exercise.Step(text=u"", validatorName=b"")
         verifyObject(imerlin.IStep, step)
