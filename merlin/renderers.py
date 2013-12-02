9e6f49107a105dc7f4e59f4d981d76d47abe1997
Laurens Van Houtven <_@lvh.cc>
2013-12-02 10:21:29 +0100

    Basic renderers

---
+++ merlin/renderers.py
@@ -0,0 +1,23 @@
+"""
+Step renderers.
+"""
+from axiom import attributes, item
+from merlin.interfaces import IRenderer
+from zope.interface import implementer
+
+
+@implementer(IRenderer)
+class StringTemplateRenderer(item.Item):
+    """
+    A renderer which will render a str.format-style template with a context.
+    """
+    template = attributes.text(allowNone=False)
+    context = attributes.reference()
+
+
+    def render(self, _userStore):
+        """
+        Formats the template using the context.
+        """
+        kwargs = dict.fromkeys(["c", "ctx", "context"], self.context)
+        return self.template.format(**kwargs)
