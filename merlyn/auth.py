ba060713f1e0a7f98d4c92f37facd190cb18334a
Laurens Van Houtven <_@lvh.cc>
2013-12-10 12:15:50 +0100

    Move context factory stuff to auth

--- merlyn/auth.py
+++ merlyn/auth.py
@@ -3,6 +3,8 @@ from axiom.errors import ItemNotFound
 from clarent.auth import Register
 from clarent.auth import BadEmailAddress, AlreadyRegistered, NotRegistered
 from functools import wraps
+from OpenSSL.SSL import Context, VERIFY_PEER, SSLv23_METHOD
+from OpenSSL.SSL import OP_SINGLE_DH_USE, OP_NO_SSLv2, OP_NO_SSLv3
 from twisted.protocols.amp import CommandLocator
 
 
@@ -80,3 +82,32 @@ def loginRequired(m):
         else:
             raise NotRegistered()
     return decorated
+
+
+
+class ContextFactory(object):
+    def getContext(self):
+        """Creates a context.
+
+        This will make contexts using ``SSLv23_METHOD``. This is
+        because OpenSSL thought it would be a good idea to have
+        ``TLSv1_METHOD`` mean "only use TLSv1.0" -- specifically, it
+        disables TLSv1.2. Since we don't want to use SSLv2 and v3, we
+        set OP_NO_SSLv2|OP_NO_SSLv3. Additionally, we set
+        OP_SINGLE_DH_USE.
+
+        """
+        ctx = Context(SSLv23_METHOD)
+        ctx.use_certificate_file("cert.pem")
+        ctx.use_privatekey_file("key.pem")
+        ctx.set_options(OP_SINGLE_DH_USE|OP_NO_SSLv2|OP_NO_SSLv3)
+        ctx.set_verify(VERIFY_PEER, _verify)
+        return ctx
+
+
+
+def _verify(connection, x509, errorNumber, errorDepth, returnCode):
+    """Always pretend the certificate verified.
+
+    """
+    return True
--- merlyn/service.py
+++ merlyn/service.py
@@ -1,7 +1,5 @@
 from axiom import store
 from merlyn import auth, exercise
-from OpenSSL.SSL import Context, VERIFY_PEER, SSLv23_METHOD
-from OpenSSL.SSL import OP_SINGLE_DH_USE, OP_NO_SSLv2, OP_NO_SSLv3
 from twisted.application import service
 from twisted import plugin
 from twisted.internet import reactor
@@ -46,36 +44,7 @@ class Service(service.Service):
     def startService(self):
         factory = ServerFactory.forProtocol(AMP)
         factory.store = self.store
-        reactor.listenSSL(4430, factory, ContextFactory())
-
-
-
-class ContextFactory(object):
-    def getContext(self):
-        """Creates a context.
-
-        This will make contexts using ``SSLv23_METHOD``. This is
-        because OpenSSL thought it would be a good idea to have
-        ``TLSv1_METHOD`` mean "only use TLSv1.0" -- specifically, it
-        disables TLSv1.2. Since we don't want to use SSLv2 and v3, we
-        set OP_NO_SSLv2|OP_NO_SSLv3. Additionally, we set
-        OP_SINGLE_DH_USE.
-
-        """
-        ctx = Context(SSLv23_METHOD)
-        ctx.use_certificate_file("cert.pem")
-        ctx.use_privatekey_file("key.pem")
-        ctx.set_options(OP_SINGLE_DH_USE|OP_NO_SSLv2|OP_NO_SSLv3)
-        ctx.set_verify(VERIFY_PEER, _verify)
-        return ctx
-
-
-
-def _verify(connection, x509, errorNumber, errorDepth, returnCode):
-    """Always pretend the certificate verified.
-
-    """
-    return True
+        reactor.listenSSL(4430, factory, auth.ContextFactory())
 
 
 
