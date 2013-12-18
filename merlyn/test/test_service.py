43656a3857a46865acd9c3c8fbbdf1335da5bd86
Laurens Van Houtven <_@lvh.cc>
2013-12-18 15:45:53 +0100

    Make tests more better

--- merlyn/test/test_service.py
+++ merlyn/test/test_service.py
@@ -1,5 +1,7 @@
 from axiom.store import Store
 from merlyn import service
+from twisted.internet.protocol import connectionDone
+from twisted.test.proto_helpers import StringTransport
 from twisted.trial.unittest import SynchronousTestCase
 
 
@@ -20,10 +22,11 @@ class AMPTests(SynchronousTestCase):
         self.assertEqual(self.factory.protocols, set([]))
 
         proto = self.factory.buildProtocol(None)
+        proto.makeConnection(StringTransport())
         proto.connectionMade()
         self.assertEqual(self.factory.protocols, set([proto]))
 
-        proto.connectionLost(None)
+        proto.connectionLost(connectionDone)
         self.assertEqual(self.factory.protocols, set([]))
 
 
@@ -40,3 +43,31 @@ class AMPTests(SynchronousTestCase):
         """
         proto = self.factory.buildProtocol(None)
         self.assertIdentical(proto.store, self.store)
+
+
+
+class ServiceMakerTests(SynchronousTestCase):
+    def test_options(self):
+        """The service maker uses the merlyn Options class.
+
+        """
+        self.assertIdentical(service.ServiceMaker.options, service.Options)
+
+
+    def test_tapname(self):
+        """The service maker uses the tapname ``merlyn``.
+
+        """
+        self.assertIdentical(service.ServiceMaker.tapname, b"merlyn")
+
+
+    def test_makeService(self):
+        """The service maker grabs the store from the options, and passes it
+        to ``service.Service``.
+
+        """
+        maker = service.ServiceMaker()
+        store = Store()
+        svc = maker.makeService({"store": store})
+        self.assertTrue(isinstance(svc, service.Service))
+        self.assertIdentical(svc.store, store)
